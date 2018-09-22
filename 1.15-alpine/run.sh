8004655bf0a3bc5b388c311f58532aa41b5fc592
Emmanuel Dyan <emmanueldyan@gmail.com>
2018-09-22 21:45:55 +0200

    fixed wrong user name for nginx

--- 1.15-alpine/run.sh
+++ 1.15-alpine/run.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-CURRENT_NGINX_ID=$(id -u nginx)
+CURRENT_NGINX_ID=$(id -u www-data)
 if [ "$CURRENT_NGINX_ID" != "$NGINX_UID" ]; then
-    echo "Fixing permissions for 'nginx'"
-    sed -i "s/:100:101:Linux User/:$NGINX_UID:$NGINX_GID:Nginx System User/g" /etc/passwd
-    sed -i "s/:101:nginx/:$NGINX_GID:nginx/g" /etc/group
-    chown -R nginx:nginx /var/www /var/log/nginx
+    echo "Fixing permissions for 'www-data'"
+    usermod  -u $NGINX_UID www-data
+    groupmod -g $NGINX_GID www-data
+    chown -R www-data:www-data /var/www /var/log/www-data
 fi
 
 envsubst '\$PHP_HOST \$PHP_PORT' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf
